9563d58153a3097e5f24f922696e510d528f3f9a
Zach Laine <whatwasthataddress@gmail.com>
2016-11-15 17:28:19 -0600

    depth_stress_test -> depth_stress_test_{left,right}

---
+++ test/depth_stress_test_right.cpp
@@ -0,0 +1,63 @@
+#define BOOST_PROTO17_CONVERSION_OPERATOR_TEMPLATE
+#include "expression.hpp"
+
+#include <gtest/gtest.h>
+
+#include <sstream>
+
+
+template <typename T>
+using term = boost::proto17::terminal<T>;
+
+namespace bp17 = boost::proto17;
+
+
+TEST(right, depth_stress_test)
+{
+    term<double> unity{1.0};
+
+    {
+        auto expr = unity + unity + unity + unity +    unity + unity + unity + unity;
+        double result = expr;
+        EXPECT_EQ(result, 8.0);
+    }
+
+    {
+        auto expr =
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity;
+        double result = expr;
+        EXPECT_EQ(result, 64.0);
+    }
+
+    {
+        auto expr =
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity +
+            unity + unity + unity + unity +    unity + unity + unity + unity;
+        double result =
+            expr + expr + expr + expr +    expr + expr + expr + expr +
+            expr + expr + expr + expr +    expr + expr + expr + expr +
+            expr + expr + expr + expr +    expr + expr + expr + expr +
+            expr + expr + expr + expr +    expr + expr + expr + expr +
+
+            expr + expr + expr + expr +    expr + expr + expr + expr +
+            expr + expr + expr + expr +    expr + expr + expr + expr +
+            expr + expr + expr + expr +    expr + expr + expr + expr +
+            expr + expr + expr + expr +    expr + expr + expr + expr;
+        EXPECT_EQ(result, 64.0 * 64.0);
+    }
+}
