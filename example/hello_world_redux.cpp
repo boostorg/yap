b76eb66101e8bc3c2a64496e3735eca6ab2986ad
Zach Laine <whatwasthataddress@gmail.com>
2016-12-11 23:32:39 -0600

    Add hello_world_redux example.

---
+++ example/hello_world_redux.cpp
@@ -0,0 +1,27 @@
+//[hello_world_redux
+#include <boost/yap/expression.hpp>
+
+#include <iostream>
+
+
+template <boost::yap::expr_kind Kind, typename Tuple>
+struct stream_expr
+{
+    static const boost::yap::expr_kind kind = Kind;
+
+    Tuple elements;
+
+    template <typename T>
+    decltype(auto) operator<< (T && x)
+    { return boost::yap::value(*this) << std::forward<T &&>(x); }
+};
+
+
+int main ()
+{
+    auto cout = boost::yap::make_terminal<stream_expr>(std::cout);
+    cout << "Hello" << ',' << " world!\n";
+
+    return 0;
+}
+//]
