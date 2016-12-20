e69d40eb5813863570d6b4f9e83ef1923b70ec2f
Zach Laine <whatwasthataddress@gmail.com>
2016-12-19 18:51:00 -0600

    Document print().

--- doc/other_snippets.cpp
+++ doc/other_snippets.cpp
@@ -1,4 +1,5 @@
 #include <boost/yap/yap.hpp>
+#include <boost/yap/print.hpp>
 
 #include <iostream>
 
@@ -40,6 +41,31 @@ namespace user {
 }
 //]
 
+//[ print_decl
+struct thing {};
+//]
+
+void print_expr ()
+{
+//[ print_expr
+using namespace boost::yap::literals;
+
+auto const const_lvalue_terminal_containing_rvalue = boost::yap::make_terminal("lvalue terminal");
+
+double const d = 1.0;
+auto rvalue_terminal_containing_lvalue = boost::yap::make_terminal(d);
+
+auto thing_terminal = boost::yap::make_terminal(thing{});
+
+auto expr =
+    4_p +
+    std::move(rvalue_terminal_containing_lvalue) * thing_terminal -
+    const_lvalue_terminal_containing_rvalue;
+//]
+
+boost::yap::print(std::cout, expr) << "\n";
+}
+
 int main ()
 {
     foo();
@@ -55,6 +81,7 @@ int main ()
     // Does not compile; there is no operator- defined over user::number.
     //user::number minus_result = evaluate(minus_expr);
 //]
+    (void)minus_expr;
     }
 
     {
@@ -63,6 +90,8 @@ int main ()
     std::cout << evaluate(expr) << "\n"; // Prints "Yay."  Wierd!
 //]
     }
+
+    print_expr();
 
     return 0;
 }
