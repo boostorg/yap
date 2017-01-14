5effa47f222f5f938b6f1226498ed4764b242e2b
Zach Laine <whatwasthataddress@gmail.com>
2017-01-13 22:21:55 -0600

    #define BOOST_NO_CONSTEXPR_IF 0 if no previous definition is given.

--- boost/yap/config.hpp
+++ boost/yap/config.hpp
@@ -15,4 +15,13 @@
 #define BOOST_YAP_CONVERSION_OPERATOR_TEMPLATE 0
 #endif
 
+#ifndef BOOST_NO_CONSTEXPR_IF
+/** Indicates whether the compiler supports constexpr if.
+
+    If the user does not define any value for this, we assume that the
+    compiler does not have the necessary support.  Note that this is a
+    temporary hack; this should eventually be a Boost-wide macro. */
+#define BOOST_NO_CONSTEXPR_IF 1
+#endif
+
 #endif
