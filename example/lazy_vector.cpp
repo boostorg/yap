8282c4b10ecf668f36b4be0c4ed173f8686d6b5d
Zach Laine <whatwasthataddress@gmail.com>
2018-02-15 17:20:48 -0600

    Add Boost license copyright headers to all C++ files.
    Fixes #31.

--- example/lazy_vector.cpp
+++ example/lazy_vector.cpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2016-2018 T. Zachary Laine
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 //[ lazy_vector
 // Defining this allows the assignment below of an expression to a double
 // without writing any specific code to do so.
--- include/boost/yap/detail/algorithm.hpp
+++ include/boost/yap/detail/algorithm.hpp
@@ -1,3 +1,8 @@
+// Copyright (C) 2016-2018 T. Zachary Laine
+//
+// Distributed under the Boost Software License, Version 1.0. (See
+// accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
 #ifndef BOOST_YAP_DETAIL_EXPRESSION_HPP_INCLUDED
 #define BOOST_YAP_DETAIL_EXPRESSION_HPP_INCLUDED
 
