123ea1df6a27b5dccd721dfcf2a63ae41208a47b
Zach Laine <whatwasthataddress@gmail.com>
2018-02-16 22:42:28 -0600

    Fix malformed expr_kind::call overload in transform_terminals example.

--- example/transform_terminals.cpp
+++ example/transform_terminals.cpp
@@ -5,13 +5,9 @@
 // http://www.boost.org/LICENSE_1_0.txt)
 //[ terminal_transforms
 #include <boost/yap/yap.hpp>
-#include <boost/yap/print.hpp> // TODO
-
-#include <vector>
-#include <iostream>
 
 
-//[ vector_take_nth_xform
+//[ iota_terminal_transform
 struct iota_terminal_transform
 {
     // Base case. Note that we're ignoring placeholders entirely for this
@@ -43,36 +39,17 @@ struct iota_terminal_transform
             boost::yap::transform(boost::yap::as_expr(expr), *this));
     }
 
-#if 1
+    template<typename CallableExpr, typename... Arg>
     auto operator()(boost::yap::expr_tag<boost::yap::expr_kind::call>,
-                    int (*callable)(int, int), int a, int b)
+                    CallableExpr callable, Arg &&... arg)
     {
-        std::cout << "====================\n"
-                  << "boost::yap::value(callable)=" << boost::yap::value(callable) << "\n";
         // Even though the callable in a call expression is technically a
         // terminal, it doesn't make a lot of sense to replace it with an int,
         // so we'll only transform the argument subexpressions.
         return boost::yap::make_expression<boost::yap::expr_kind::call>(
             boost::yap::as_expr(callable),
-            boost::yap::transform(boost::yap::as_expr(a), *this),
-            boost::yap::transform(boost::yap::as_expr(b), *this));
-    }
-#else // TODO: Is not picked up; needs a fix.
-    // Recursive case: Match any call expression.
-    template<boost::yap::expr_kind Kind, typename CallableExpr, typename... Arg>
-    auto operator()(boost::yap::expr_tag<boost::yap::expr_kind::call>,
-                    CallableExpr && callable, Arg &&... arg)
-    {
-        std::cout << "++++++++++++++++++++\n"
-                  << "boost::yap::value(callable)=" << boost::yap::value(callable) << "\n";
-        // Even though the callable in a call expression is technically a
-        // terminal, it doesn't make a lot of sense to replace it with an int,
-        // so we'll only transform the argument subexpressions.
-        return boost::yap::make_expression<boost::yap::expr_kind::call>(
-            std::forward<CallableExpr>(callable),
             boost::yap::transform(boost::yap::as_expr(arg), *this)...);
     }
-#endif
 
     // if_else/ternary added as necessary.
 
@@ -98,6 +75,14 @@ int main()
 
         auto iota_expr = boost::yap::transform(expr, iota_terminal_transform{0});
         assert(evaluate(iota_expr) == -1);
+    }
+
+    {
+        auto expr = boost::yap::make_terminal(sum)(-(boost::yap::make_terminal(8) + 8), 0);
+        assert(evaluate(expr) == -16);
+
+        auto iota_expr = boost::yap::transform(expr, iota_terminal_transform{0});
+        assert(evaluate(iota_expr) == -3);
     }
 }
 //]
