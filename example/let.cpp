a562edb346a4fadba73673dad858cef1dc5fe0f7
Zach Laine <whatwasthataddress@gmail.com>
2018-02-24 23:58:41 -0600

    Flesh out commentary in let.cpp; add it to the docs as an example.

--- example/let.cpp
+++ example/let.cpp
@@ -3,6 +3,7 @@
 // Distributed under the Boost Software License, Version 1.0. (See
 // accompanying file LICENSE_1_0.txt or copy at
 // http://www.boost.org/LICENSE_1_0.txt)
+//[ let
 #include <boost/yap/yap.hpp>
 
 #include <boost/hana/map.hpp>
@@ -14,26 +15,29 @@
 #include <iostream>
 
 
+// Here, we introduce special let-placeholders, so we can use them along side
+// the normal YAP placeholders without getting them confused.
 template<long long I>
 struct let_placeholder : boost::hana::llong<I>
 {
 };
 
+// Replaces each let-terminal with the expression with which it was
+// initialized in let().  So in 'let(_a = foo)[ _a + 1 ]', this transform will
+// be used on '_a + 1' to replace '_a' with 'foo'.  The map_ member holds the
+// mapping of let-placeholders to their initializers.
 template<typename ExprMap>
 struct let_terminal_transform
 {
-    template<typename T>
-    auto
-    operator()(boost::yap::expr_tag<boost::yap::expr_kind::terminal>, T && t)
-    {
-        return boost::yap::as_expr(std::forward<T>(t));
-    }
-
+    // This matches only let-placeholders.  For each one matched, we look up
+    // its initializer in map_ and return it.
     template<long long I>
     auto operator()(
         boost::yap::expr_tag<boost::yap::expr_kind::terminal>,
         let_placeholder<I> i)
     {
+        // If we have an entry in map_ for this placeholder, return the value
+        // of the entry.  Otherwise, pass i through as a terminal.
         if constexpr (boost::hana::contains(
                           decltype(boost::hana::keys(map_))(),
                           boost::hana::llong_c<I>)) {
@@ -43,26 +47,15 @@ struct let_terminal_transform
         }
     }
 
-    template<boost::yap::expr_kind Kind, typename... Args>
-    auto operator()(boost::yap::expr_tag<Kind>, Args &&... args)
-    {
-        return boost::yap::make_expression<Kind>(
-            boost::yap::transform(args, *this)...);
-    }
-
-    template<typename CallableExpr, typename... Arg>
-    auto operator()(
-        boost::yap::expr_tag<boost::yap::expr_kind::call>,
-        CallableExpr callable,
-        Arg &&... arg)
-    {
-        return boost::yap::make_expression<boost::yap::expr_kind::call>(
-            callable, boost::yap::transform(arg, *this)...);
-    }
-
     ExprMap map_;
 };
 
+// As you can see below, let() is an eager function; this template is used for
+// its return values.  It contains the mapping from let-placeholders to
+// initializer expressions used to transform the expression inside '[]' after
+// a let()'.  It also has an operator[]() member function that takes the
+// expression inside '[]' and returns a version of it with the
+// let-placeholders replaced.
 template<typename ExprMap>
 struct let_result
 {
@@ -76,6 +69,8 @@ struct let_result
     ExprMap map_;
 };
 
+// Processes the expressions passed to let() one at a time, adding each one to
+// a Hana map of hana::llong<>s to YAP expressions.
 template<typename Map, typename Expr, typename... Exprs>
 auto let_impl(Map && map, Expr && expr, Exprs &&... exprs)
 {
@@ -101,6 +96,8 @@ auto let_impl(Map && map, Expr && expr, Exprs &&... exprs)
     }
 }
 
+// Takes N > 0 expressions of the form 'placeholder = expr', and returns an
+// object with an overloaded operator[]().
 template<typename Expr, typename... Exprs>
 auto let(Expr && expr, Exprs &&... exprs)
 {
@@ -112,6 +109,8 @@ auto let(Expr && expr, Exprs &&... exprs)
 
 int main()
 {
+    // Some handy terminals -- the _a and _b let-placeholders and std::cout as
+    // a YAP terminal.
     boost::yap::expression<
         boost::yap::expr_kind::terminal,
         boost::hana::tuple<let_placeholder<0>>> const _a;
@@ -132,6 +131,8 @@ int main()
         assert(boost::yap::evaluate(expr) == 123 + 456);
     }
 
+    // This prints out "0 0", because 'i' is passed as an lvalue, so its
+    // decrement is visible outside the let expression.
     {
         int i = 1;
 
@@ -140,18 +141,37 @@ int main()
         std::cout << i << std::endl;
     }
 
+    // Prints "Hello, World" due to let()'s scoping rules.
     {
         boost::yap::evaluate(
             let(_a = 1_p, _b = 2_p)
             [
                 // _a here is an int: 1
 
-               let(_a = 3_p) // hides the outer _a
-               [
-                   cout << _a << _b // prints "Hello, World"
-               ]
+                let(_a = 3_p) // hides the outer _a
+                [
+                    cout << _a << _b // prints "Hello, World"
+                ]
             ],
             1, " World", "Hello,"
         );
     }
+
+    std::cout << "\n";
+
+    // Due to the macro-substitution style that this example uses, this prints
+    // "3132".  Phoenix's let() prints "312", because it only evaluates '1_p
+    // << 3' once.
+    {
+        boost::yap::evaluate(
+            let(_a = 1_p << 3)
+            [
+                _a << "1", _a << "2"
+            ],
+            std::cout
+        );
+    }
+
+    std::cout << "\n";
 }
+//]
