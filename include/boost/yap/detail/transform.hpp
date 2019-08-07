04361f040eeb4b32e1956d01b808ff8cf32641b2
Paul Keir <pkeir@outlook.com>
2019-08-07 12:53:20 +0100

    Fixed an bug in placeholder_transform_t::operator() which used a non-existent value member of rvalue_mover. This allows replacements() and replace_placeholders() to be used. Also added a test file which checks these two, as well as evaluation().

--- include/boost/yap/detail/transform.hpp
+++ include/boost/yap/detail/transform.hpp
@@ -85,9 +85,8 @@ namespace boost { namespace yap { namespace detail {
                 I <= decltype(hana::size(std::declval<tuple_t>()))::value,
                 "Out of range placeholder index,");
             using nth_type = nth_element<I - 1, PlaceholderArgs...>;
-            return as_expr<minimal_expr>(
-                rvalue_mover<!std::is_lvalue_reference<nth_type>::value>::value(
-                    placeholder_args_[hana::llong<I - 1>{}]));
+            return rvalue_mover<!std::is_lvalue_reference<nth_type>::value>{}(
+                placeholder_args_[hana::llong<I - 1>{}]);
         }
 
         tuple_t placeholder_args_;
